46734de00ef34ebe62087582753d73dd83365e40
Matt Burleigh <mburleigh@pandora.net>
2019-05-31 13:10:13 +0000

    use variables instead of ordinal parameters

--- AzDO/createAciContainer.bat
+++ AzDO/createAciContainer.bat
@@ -1,39 +1,45 @@
-set containerRegistry=%1
-REM echo "container registry:" %containerRegistry%
-REM echo "parameter 2:" %2
+set imageRegistry=%1
+REM echo "image registry:" %imageRegistry%
+set imageRepository=%2
+REM echo "image repository:" %imageRepository%
+set imageName=%3
+REM echo "image name:" %imageName%
+set id=%4
+REM echo "id:" %id%
 
-call az acr update -n %containerRegistry% --admin-enabled true
+call az acr update -n %imageRegistry% --admin-enabled true
 
-call az acr show --name %containerRegistry% --query loginServer --output tsv > tmp
+call az acr show --name %imageRegistry% --query loginServer --output tsv > tmp
 set /p loginserver= < tmp
 REM echo "login server:" %loginserver%
 
-call az acr credential show --name %containerRegistry% --query "username" --output tsv > tmp
+call az acr credential show --name %imageRegistry% --query "username" --output tsv > tmp
 set /p username= < tmp
 REM echo "user name:" %username%
 
-call az acr credential show --name %containerRegistry% --query "passwords[0].value" --output tsv > tmp
+call az acr credential show --name %imageRegistry% --query "passwords[0].value" --output tsv > tmp
 set /p password= < tmp
 REM echo "password:" %password%
 
-call az acr show --name %containerRegistry% --query "resourceGroup" --output tsv > tmp
+call az acr show --name %imageRegistry% --query "resourceGroup" --output tsv > tmp
 set /p resourcegroup= < tmp
 REM echo "resource group:" %resourcegroup%
 
-set imagerepository=%4
-set image=%loginserver%/%imagerepository%:%2
+set image=%loginserver%/%imagerepository%:%imageName%
 echo "image:" %image%
 
-call az container create -g %resourcegroup% -n %2 --image %image% --cpu 1 --memory 1 --registry-login-server %loginserver% --registry-username %username% --registry-password %password% --dns-name-label %2-%3 --ports 80 --os-type Windows
+call az container create -g %resourcegroup% -n %imageName% --image %image% --cpu 1 --memory 1^
+  --registry-login-server %loginserver% --registry-username %username% --registry-password %password%^
+  --dns-name-label %imageName%-%id% --ports 80 --os-type Windows
 
-call az container exec -g %resourcegroup% -n %2 --exec-command powershell 
+call az container exec -g %resourcegroup% -n %imageName% --exec-command powershell 
 
 $nic = Get-NetAdapter ; Set-DnsClientServerAddress -InterfaceIndex $nic.IfIndex -ServerAddresses ('1.1.1.1','8.8.8.8');
 exit
 
-call az container restart -g %resourcegroup% -n %2 --no-wait
+call az container restart -g %resourcegroup% -n %imageName% --no-wait
 
-call az container show --resource-group %resourcegroup% --name %2 --query ipAddress.fqdn --output tsv > tmp
+call az container show --resource-group %resourcegroup% --name %imageName% --query ipAddress.fqdn --output tsv > tmp
 set /p url= < tmp
 echo "url:" %url%
 
