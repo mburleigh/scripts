e7f62f4afd8bbae9e2aae1ddef5dc960e75457d3
mburleigh <matt.burleigh@gmail.com>
2019-06-05 14:01:02 -0400

    add acrAuth parameter

--- Slack/blockNotificationQaContainer.ps1
+++ Slack/blockNotificationQaContainer.ps1
@@ -1,7 +1,7 @@
 # this script should be included in a build pipeline using a PowerShell script task
 
 param (
-    $message,           # Build.SourceVersionMessage
+    $message,           # notification message (typically Build.SourceVersionMessage)
     $branch,            # Build.SourceBranch
     $username,          # the user name that shows on the Slack message
     $webhookuri,        # the Slack web hook to call
@@ -12,6 +12,7 @@ param (
     $title,             # the title that will be presented on the confirmation web page
     $acrRegistry,       # the target Azure Container Registry
     $acrRepository,     # the target repository in the acrRegistry
+    $acrAuth,           # authentication token for target repository in the acrRegistry
     $aciResourceGroup,  # the target resource group for the ACI instance of the image
     $project,           # the team project
     $targetBranch,      # the target branch for the merge
@@ -46,7 +47,7 @@ else
 {
     # these query string parameters match the inputs to the cleanup function
     $url = [uri]::EscapeUriString("$cleanupUrl&title=$title&container=$containerUrl&image=$imageName&buildId=$buildId&"+
-      "acrRegistry=$acrRegistry&acrRepository=$acrRepository&aciResourceGroup=$aciResourceGroup&"+
+      "acrRegistry=$acrRegistry&acrRepository=$acrRepository&acrAuth=$acrAuth&aciResourceGroup=$aciResourceGroup&"+
       "project=$project&targetBranch=$targetBranch&team=$team&callbackUrl=$callbackUrl")
     #Write-Host $url
 
