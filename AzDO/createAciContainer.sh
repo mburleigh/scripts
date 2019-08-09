f44d0e882bce6be5849b8506d98a1f3ea44765f8
Matt Burleigh <mburleigh@pandora.net>
2019-08-09 20:06:42 +0000

    Updated createAciContainer.sh

--- AzDO/createAciContainer.sh
+++ AzDO/createAciContainer.sh
@@ -1,28 +1,28 @@
 imageRegistry=$1
-#echo "image registry:" $imageRegistry
+echo "image registry:" $imageRegistry
 imageRepository=$2
-#echo "image repository:" $imageRepository
+echo "image repository:" $imageRepository
 imageName=$3
-#echo "image name:" $imageName
+echo "image name:" $imageName
 id=$4
-#echo "id:" $id
+echo "id:" $id
 
 az acr update -n $imageRegistry --admin-enabled true
 
-loginserver=$(az acr show -n $imageRegistry --query loginServer --output tsv)
-#echo "login server:" $loginserver
+loginserver=$(az acr show -n $imageRegistry --query loginServer -o tsv)
+echo "login server:" $loginserver
 
-username=$(az acr credential show -n $imageRegistry --query "username" --output tsv)
-#echo "user name:" $username
+username=$(az acr credential show -n $imageRegistry --query "username" -o tsv)
+echo "user name:" $username
 
-password=$(az acr credential show -n $imageRegistry --query "passwords[0].value" --output tsv)
-#echo "password:" $password
+password=$(az acr credential show -n $imageRegistry --query "passwords[0].value" -o tsv)
+echo "password:" $password
 
-resourcegroup=$(az acr show -n $imageRegistry --query "resourceGroup" --output tsv)
-#echo "resource group:" $resourcegroup
+resourcegroup=$(az acr show -n $imageRegistry --query "resourceGroup" -o tsv)
+echo "resource group:" $resourcegroup
 
 image=$loginserver/$imageRepository:$imageName
-#echo "image:" $image
+echo "image:" $image
 
 az container create -g $resourcegroup -n $imageName --image $image --cpu 1 --memory 1 --registry-login-server \
   $loginserver --registry-username $username --registry-password $password --dns-name-label $imageName-$id --ports 80
