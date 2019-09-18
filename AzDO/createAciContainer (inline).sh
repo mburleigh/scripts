5ce3ea564aa4681c3c6f8c69006a51c211abaa11
Matthew Burleigh <matt.burleigh@gmail.com>
2019-09-18 11:16:53 -0400

    Create createAciContainer (inline).sh

---
+++ AzDO/createAciContainer (inline).sh
@@ -0,0 +1,36 @@
+imageRegistry=$1
+echo "image registry:" $imageRegistry
+imageRepository=$2
+echo "image repository:" $imageRepository
+imageName=$3
+echo "image name:" $imageName
+id=$4
+echo "id:" $id
+
+az acr update -n $imageRegistry --admin-enabled true
+
+loginserver=$(az acr show -n $imageRegistry --query loginServer -o tsv)
+echo "login server:" $loginserver
+
+username=$(az acr credential show -n $imageRegistry --query "username" -o tsv)
+echo "user name:" $username
+
+password=$(az acr credential show -n $imageRegistry --query "passwords[0].value" -o tsv)
+echo "password:" $password
+
+resourcegroup=$(az acr show -n $imageRegistry --query "resourceGroup" -o tsv)
+echo "resource group:" $resourcegroup
+
+image=$loginserver/$imageRepository:$imageName
+echo "image:" $image
+
+az container create -g $resourcegroup -n $imageName --image $image --cpu 1 --memory 1 --registry-login-server \
+  $loginserver --registry-username $username --registry-password $password --dns-name-label $imageName-$id --ports 80
+
+az container restart -g $resourcegroup -n $imageName --no-wait
+
+url=$(az container show -g $resourcegroup -n $imageName --query ipAddress.fqdn -o tsv)
+echo "url:" $url
+echo "##vso[task.setvariable variable=containerUrl]$url"
+
+echo "Done!!!"
